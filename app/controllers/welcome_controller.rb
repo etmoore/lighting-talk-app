9935bc155595217a26546537d4703c236ab9ac7e
Andrew Deitrick <ajdeitrick@gmail.com>
2015-02-28 17:15:33 -0700

    rename static pages to welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,4 @@
-class StaticPagesController < ApplicationController
+class WelcomeController < ApplicationController
   skip_before_action :ensure_current_user
   def index
     @days = Day.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  root 'welcome#index'
   get '/auth/github/callback', to: 'authentications#create'
   get '/auth/github', as: 'sign-in'
   get '/sign-out', to: 'authentications#destroy'
