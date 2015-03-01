b5cc5f4df0b56968b7e0b3ab6c6f7af3f13c7e76
Andrew Deitrick <ajdeitrick@gmail.com>
2015-02-28 17:14:35 -0700

    ensure current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :ensure_current_user
 
   helper_method :current_user
 
   def current_user
     User.find_by(auth_token: session[:access_token])
+  end
+
+  private
+
+  def ensure_current_user
+    redirect_to root_path, notice: "Please login to see that page" unless current_user.present?
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :ensure_current_user
   def index
     @days = Day.all
     @classes = ["success", "info", "danger", "warning", "active"]
