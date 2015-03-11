705cd5f2bca9aa215ff92a3ced21938e62a0fbd6
Dylan Ham <dylanham@gmail.com>
2015-03-10 22:13:53 -0600

    update days in new to allow for selection of all dates from today and on

--- app/controllers/admin/lightning_talks_controller.rb
+++ app/controllers/admin/lightning_talks_controller.rb
@@ -8,19 +8,20 @@ class Admin::LightningTalksController < ApplicationController
 
   def new
     @lightning_talk = LightningTalk.new
-    @days = Day.where("talk_date >=?", Time.now).sort_by { |d| d.talk_date }
+    @days = Day.where("talk_date >=?", Date.today).sort_by { |d| d.talk_date }
     @users = User.all.sort_by { |u| u.username.downcase }
   end
 
   def create
     lightning_talk = LightningTalk.new(lightning_talk_params)
     lightning_talk, success = LightningTalkManager.build(lightning_talk)
-    @days = Day.all
+    @days = Day.where("talk_date >=?", Date.today).sort_by { |d| d.talk_date}
     @users = User.all
     if lightning_talk.save
       flash[:notice] = "Lightning Talk Successfully Created"
       redirect_to admin_lightning_talks_path
     else
+      flash[:notice] = "Somethign went wrong"
       @lightning_talk = lightning_talk
       render :new
     end
