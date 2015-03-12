a3a467075a0ea0d13469793363118c45ff390d59
Dylan Ham <dylanham@gmail.com>
2015-03-12 08:23:13 -0600

    ensure_current_talk_owner in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,12 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, notice: "Please login to see that page" unless current_user.present?
   end
 
+  def ensure_current_talk_owner
+    redirect_to root_path, notice: "You don't have permission to access that page" unless current_user.id == @lightning_talk.user_id
+  end
+
   def require_admin
     redirect_to root_path, notice: "You don't have permission to access that page" unless current_user && current_user.admin
   end
-  
+
 end
