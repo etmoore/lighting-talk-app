22e3515ed9e7a11699d1968f5bdbba0ed3728335
Dylan Ham <dylanham@gmail.com>
2015-03-17 15:10:26 -0600

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301060708) do
+ActiveRecord::Schema.define(version: 20150312193828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150301060708) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "talk_idea_id"
   end
 
   create_table "talk_ideas", force: :cascade do |t|
